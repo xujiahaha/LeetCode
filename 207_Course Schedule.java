ee5098946f69f4080aa771eebb8972830ee9b0db
xujiahaha <xujiahaha@hotmail.com>
2017-06-12 23:10:54 -1000

    added DFS solution

--- 207_Course Schedule.java
+++ 207_Course Schedule.java
@@ -39,3 +39,46 @@ public class Solution {
         return count == numCourses;
     }
 }
+// DFS solution: check if there is a cycle in the graph.
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(prerequisites == null || prerequisites.length == 0 || prerequisites[0].length == 0) return true;
+        List[] adj = new List[numCourses];
+        // intialize adjacency list
+        for(int i = 0; i < numCourses; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        // fill out the adjacency list and get indegree of every course
+        for(int i = 0; i < prerequisites.length; i++) {
+            int cur = prerequisites[i][0];
+            int prev = prerequisites[i][1];
+            adj[prev].add(cur);
+        }
+        // status is used to mark the status of one node. there are 3 status: 
+        // 1) visited, marked as -1; 2) unvisited, marked as 0; 3) on current path, marked as 1
+        int[] status = new int[numCourses];
+        for(int i = 0; i < numCourses; i++) {
+            // find the entrance of a path
+            if(status[i] == 0) {
+                // function dfs() will return true if there is a cycle in the graph
+                if(dfs(i, adj, status)) return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean dfs(int cur, List[] adj, int[] status) {
+        // mark cur is on current path
+        status[cur] = 1;
+        for(int i = 0; i < adj[cur].size(); i++) {
+            int neighbor = (int) adj[cur].get(i);
+            if(status[neighbor] == 1 || dfs(neighbor, adj, status)) {
+                return true;
+            } 
+        }
+        // no cycle, mark cur as visited
+        status[cur] = -1; // do not forget this line
+        return false;
+    }
+    
+}
--- 210_Course Schedule II.java
+++ 210_Course Schedule II.java
@@ -40,3 +40,46 @@ public class Solution {
         return count == numCourses ? res : new int[0];
     }
 }
+// DFS solution
+public class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List[] adj = new List[numCourses];
+        // intialize adjacency list
+        for(int i = 0; i < numCourses; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        // fill out the adjacency list and get indegree of every course
+        for(int i = 0; i < prerequisites.length; i++) {
+            int cur = prerequisites[i][0];
+            int prev = prerequisites[i][1];
+            adj[prev].add(cur);
+        }
+        Stack<Integer> stack = new Stack<>();
+        int[] status = new int[numCourses];
+        for(int i = 0; i < numCourses; i++) {
+            if(status[i] == 0) {
+                if(dfs(adj, i, status, stack)) return new int[0];
+            }
+        }
+        int[] res = new int[numCourses];
+        int index = 0;
+        while(!stack.isEmpty()) {
+            res[index++] = stack.pop();
+        }
+        return res;
+    }
+    // return true if there is a cycle
+    public boolean dfs(List[] adj, int cur, int[] status, Stack<Integer> stack) {
+        // mark cur on current path
+        status[cur] = 1;
+        for(int i = 0; i < adj[cur].size(); i++) {
+            int neighbor = (int) adj[cur].get(i);
+            if(status[neighbor] == -1) continue;
+            if(status[neighbor] == 1 || dfs(adj, neighbor, status, stack)) return true;
+        }
+        // mark cur as visited
+        status[cur] = -1;
+        stack.push(cur);
+        return false;
+    }
+}
